45e546940d0f9bdc88f56293f995b2bde0e3aa38
ujwal <cooloozewall@gmail.com>
2018-06-24 18:16:13 +0545

    First commit

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <section id="pre_header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <a href="#"><img src="../assets/img/logo.png" alt="logo" class="img-fluid"></a>
+          </div>
+          <div class="col-md-8 text-right">
+            <ul class="list-inline">
+              <li class="list-inline-item"><button class="btn btn-primary pull-right add_space" data-toggle="modal" data-target=".bd-example-modal-lg">+</button></li>
+              <li class="list-inline-item"><a href="#">Help</a></li>
+              <li class="list-inline-item"><a href="#">Notifications</a></li>
+              <li class="list-inline-item"><a href="#">R</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- header -->
+
+    <header>
+      <nav class="navbar navbar-expand-md">
+        <div class="container">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls=" navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                           <span class="navbar-toggler-icon"><i class="fa fa-bars"></i></span>
+                      </button>
+          <a class="navbar-brand d-block d-md-none" href="#"><img src="../assets/img/logo.png" alt="..." class="img-fluid"></a>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item d-none d-md-block">
+                <a class="nav-link" href="#"><img src="../assets/img/bars.png" alt="bars"></a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Space Names</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Dashboard</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link sign_up" href="#">Settings</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav d-block d-md-none">
+              <li class="nav-item"><a href="#" class="nav-link">Help</a></li>
+              <li class="nav-item"><a href="#" class="nav-link">Notifications</a></li>
+              <li class="nav-item"><a href="#" class="nav-link big_r">R</a></li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <div class="input-group mt-2">
+                  <input type="text" class="form-control">
+                  <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button">Search</button>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  </div>
+</template>
---
+++ client/src/components/services.vue
@@ -0,0 +1,67 @@
+<template>
+<div id="services" class="w-100  sidebar">
+  <div class="container">
+    <div class="card bg-color-black" style="height:30px;margin-bottom:10px">
+      <p style="color:white;">Services</p>
+    </div>
+    <!-- service card is repeated -->
+    <div class="scrollable">
+    <!-- First service card start-->
+    <div class="card service-card" style="background-color:#415064">
+        <div class="row">
+          <div class="text-left col-md-10">
+            <h4 class="card-title color-white">Payment service</h4>
+          </div>
+          <div class="text-right col-md-2">
+            <button type="button" class="close color-white" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+          </div>
+        </div>
+
+        <h1 class="text-left color-orange">46</h1>
+        <div class="row">
+          <p class="text-left col-md-5 color-white">Now serving</p>
+          <a href="#" class="link-green w-25 h-25">Open</a>
+          <a href="#" class="link-orange w-25 h-25">Close</a>
+        </div>
+    </div>
+    <!-- End of first service card -->
+    <!-- Start of second service card -->
+    <div class="card service-card" style="background-color:#415064">
+        <div class="row">
+          <div class="text-left col-md-10">
+            <h4 class="card-title color-white">Payment service</h4>
+          </div>
+          <div class="text-right col-md-2">
+            <button type="button" class="close color-white" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+          </div>
+        </div>
+
+        <h1 class="text-left color-orange">46</h1>
+        <div class="row">
+          <p class="text-left col-md-5 color-white">Now serving</p>
+          <a href="#" class="link-green w-25 h-25">Open</a>
+          <a href="#" class="link-orange w-25 h-25">Close</a>
+        </div>
+    </div>
+    <!-- End second service card -->
+  </div>
+  <!-- Scrollable end -->
+  </div>
+
+</div>
+</template>
+<script>
+export default {
+  name: "Services",
+  data: () => ({
+
+  })
+}
+</script>
+<style>
+
+</style>
